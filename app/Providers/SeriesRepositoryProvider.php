f1c9c4e8df6877aa4ebaf0b25980a1061a82fffb
Everton000 <evertonmartins000@hotmail.com>
2022-08-04 23:19:56 -0300

    Implementações: SeriesRepository, interface na SeriesRepository, SeriesRepositoryProvider e lidando com injeção de dependências

---
+++ app/Providers/SeriesRepositoryProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\SeriesRepository;
+use App\Repositories\EloquentSeriesRepository;
+
+class SeriesRepositoryProvider extends ServiceProvider
+{
+    public array $bindings = [
+        SeriesRepository::class => EloquentSeriesRepository::class
+    ];
+}
