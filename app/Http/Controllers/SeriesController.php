58d5815ccb67cde833ef1303e22ac3d53a6b4ab2
Everton000 <evertonmartins000@hotmail.com>
2022-09-08 00:05:23 -0300

    Autenticação

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Middleware\Autenticador;
 use App\Models\Series;
 use App\Models\Episode;
 use App\Models\Season;
@@ -14,6 +15,7 @@ class SeriesController extends Controller
 {
     public function __construct(private SeriesRepository $repository)
     {
+        $this->middleware(Autenticador::class)->except('index');
     }
 
     public function index()
