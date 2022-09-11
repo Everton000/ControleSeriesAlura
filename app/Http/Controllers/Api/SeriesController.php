7c324d56309119c7dd31f506db6de9a562579dcd
Everton000 <evertonmartins000@hotmail.com>
2022-09-11 16:22:53 -0300

    CRUD Séries

--- app/Http/Controllers/Api/SeriesController.php
+++ app/Http/Controllers/Api/SeriesController.php
@@ -4,12 +4,44 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\Series;
+use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
     public function index()
     {
         return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        return response()
+            ->json($this->seriesRepository->add($request), Response::HTTP_CREATED);
+    }
+
+    public function show(int $series)
+    {
+        return Series::whereId($series)
+            ->with('season.episodes')
+            ->first();
+    }
+
+    public function update(int $series, SeriesFormRequest $request)
+    {
+        Series::where('id', $series)->update($request->all());
+        return response()->noContent();
+    }
+
+    public function destroy(int $series)
+    {
+        Series::destroy($series);
+        return response()->noContent();
     }
 }
